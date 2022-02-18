78960383746e5cb59e3ae6b01726d4d0e4b0ce25
juanwmedia <juan@wmedia.es>
2022-02-18 15:05:56 +0100

    GitHub API users fetch

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 const API = "https://api.github.com/users/";
 
+async function doSearch() {
+  const response = await fetch(API + 'juanwmedia')
+  const data = await response.json()
+  console.log(data)
+}
+
 const app = Vue.createApp({
   data() {
     return {
