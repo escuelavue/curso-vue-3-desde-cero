53a272f929ac9f35fb309a5623e8c38fdd75fe4e
juanwmedia <juan@wmedia.es>
2022-02-23 13:36:18 +0100

    2way data binding

--- js/main.js
+++ js/main.js
@@ -5,12 +5,12 @@ const API = "https://api.github.com/users/";
 const app = Vue.createApp({
   data() {
     return {
-      message: "Hello Vue!",
+      search: null
     };
   },
   methods: {
     async doSearch() {
-      const response = await fetch(API + 'juanwmedia')
+      const response = await fetch(API + this.search)
       const data = await response.json()
       console.log(data)
     }
