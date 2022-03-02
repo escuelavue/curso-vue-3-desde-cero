615a6023e26c53a3d21784bc58b72a5af99acac5
juanwmedia <juan@wmedia.es>
2022-03-02 17:07:00 +0100

    Conditional rendering

--- js/main.js
+++ js/main.js
@@ -5,14 +5,25 @@ const API = "https://api.github.com/users/";
 const app = Vue.createApp({
   data() {
     return {
-      search: null
+      search: null,
+      result: null,
+      error: null,
     };
   },
   methods: {
     async doSearch() {
-      const response = await fetch(API + this.search)
-      const data = await response.json()
-      console.log(data)
+      this.result = this.error = null
+      try {
+        const response = await fetch(API + this.search)
+        if (!response.ok) throw new Error("User not found")
+        const data = await response.json()
+        console.log(data)
+        this.result = true
+      } catch(error) {
+        this.error = error
+      } finally {
+        this.search = null
+      }
     }
   }
 });
