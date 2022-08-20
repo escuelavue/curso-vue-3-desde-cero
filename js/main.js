febafb91d0ff15fb83a397ebfe20cbfac050e1b6
juanwmedia <juan@wmedia.es>
2022-08-20 15:19:36 +0200

    Class and style bindings

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const API = "https://api.github.com/users/";
+const API = 'https://api.github.com/users/';
 
-const requestMaxTimeMs = 3000
+const requestMaxTimeMs = 3000;
 
 const app = Vue.createApp({
   data() {
@@ -8,78 +8,91 @@ const app = Vue.createApp({
       search: null,
       result: null,
       error: null,
-      favorites: new Map()
+      favorites: new Map(),
     };
   },
   created() {
-    const savedFavorites = JSON.parse(window.localStorage.getItem("favorites"))
+    const savedFavorites = JSON.parse(window.localStorage.getItem('favorites'));
     if (savedFavorites?.length) {
-      const favorites = new Map(savedFavorites.map(favorite => [favorite.login, favorite]))
-      this.favorites = favorites
+      const favorites = new Map(
+        savedFavorites.map((favorite) => [favorite.login, favorite])
+      );
+      this.favorites = favorites;
     }
   },
   computed: {
     isFavorite() {
-      return this.favorites.has(this.result.login)
+      return this.favorites.has(this.result.login);
     },
     allFavorites() {
-      return Array.from(this.favorites.values())
-    }
+      return Array.from(this.favorites.values());
+    },
   },
   methods: {
     async doSearch() {
-      this.result = this.error = null
-
-      const foundInFavorites = this.favorites.get(this.search)
+      const foundInFavorites = this.favorites.get(this.search);
 
       const shouldRequestAgain = (() => {
         if (!!foundInFavorites) {
-          const { lastRequestTime } = foundInFavorites
-          const now = Date.now()
-          return (now - lastRequestTime) > requestMaxTimeMs
+          const { lastRequest } = foundInFavorites;
+          return (
+            new Date().getTime() - new Date(lastRequest).getTime() >
+            requestMaxTimeMs
+          );
         }
-        return false
-      })() // IIFE
+        return false;
+      })(); // IIFE
 
       if (!!foundInFavorites && !shouldRequestAgain) {
-        console.log("Found and we use the cached version")
-        return this.result = foundInFavorites
+        console.log('Found and we use the cached version');
+        return (this.result = foundInFavorites);
       }
 
+      await this.doRequest()
+      if (foundInFavorites) foundInFavorites.lastRequest = new Date();
+    },
+
+    async doRequest() {
       try {
-        console.log("Not found or cached version is too old")
-        const response = await fetch(API + this.search)
-        if (!response.ok) throw new Error("User not found")
-        const data = await response.json()
-        console.log(data)
-        this.result = data
-        foundInFavorites.lastRequestTime = Date.now()
-      } catch(error) {
-        this.error = error
+        console.log('Not found or cached version is too old');
+        this.result = this.error = null;
+        const response = await fetch(API + this.search);
+        if (!response.ok) throw new Error('User not found');
+        const data = await response.json();
+        this.result = data;
+      } catch (error) {
+        this.error = error;
       } finally {
-        this.search = null
+        this.search = null;
       }
     },
 
     addFavorite() {
-      this.result.lastRequestTime = Date.now()
-      this.favorites.set(this.result.login, this.result)
-      this.updateStorage()
+      this.result.lastRequest = new Date();
+      this.favorites.set(this.result.login, this.result);
+      this.updateStorage();
     },
 
     removeFavorite() {
-      this.favorites.delete(this.result.login)
-      this.updateStorage()
+      this.favorites.delete(this.result.login);
+      this.updateStorage();
     },
 
     showFavorite(favorite) {
-      this.result = favorite
+      this.result = favorite;
+    },
+
+    checkFavorite(id) {
+      return this.result?.login === id
     },
 
     updateStorage() {
-      window.localStorage.setItem('favorites', JSON.stringify(this.allFavorites))
-    }
-  }
+      window.localStorage.setItem(
+        'favorites',
+        JSON.stringify(this.allFavorites)
+      );
+    },
+  },
 });
 
-const mountedApp = app.mount("#app")
+const mountedApp = app.mount('#app');
